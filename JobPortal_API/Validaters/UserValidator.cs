1983f6f2faf69451de5be87a6a7ec84c7ac7f431
Shivam Valand <147402134+Shivam93294Valand@users.noreply.github.com>
2025-07-28 18:05:23 +0530

    Add files via upload

---
+++ JobPortal_API/Validaters/UserValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using JobPortalAPI.Models;
+
+namespace JobPortalAPI.Validaters
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty().WithMessage("First name is required.").Equal("Naimish");
+            RuleFor(x => x.LastName).NotEmpty().NotEqual("Patel");
+            RuleFor(x => x.Password).NotEmpty().NotNull().Length(8, 20).WithMessage("Password must be between 8 and 20 characters.");
+        }
+    }
+}
